e4db513ba3ef03003b3f6757d35623b176162c95
Maic Miller <maicmiller42@gmail.com>
2021-08-25 20:40:05 -0300

    test(isChrome):add test shouldLoggedUser

---
+++ disneyPlus/src/test/java/con/disneyPlusTests/LoginDisneyPlusTests.java
@@ -0,0 +1,25 @@
+package con.disneyPlusTests;
+
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.WebDriverRunner.isChrome;
+
+public class LoginDisneyPlusTests {
+
+    @Test
+    public void shouldLoggerUser() {
+
+        isChrome();
+        open("https://www.disneyplus.com/pt-br");
+        $(".nav-initial .wrapper a").shouldHave(text("Entrar")).click();
+
+        $("#email").setValue("johnwick@gmail.com");
+        $("button[data-gv2elementkey]").click();
+
+        $("#password").setValue("johnwick@pencil");
+        $("button[name=dssLoginSubmit]").click();
+    }
+}
