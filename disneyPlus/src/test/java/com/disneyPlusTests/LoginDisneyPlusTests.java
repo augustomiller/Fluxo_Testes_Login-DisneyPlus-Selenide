fb49f68c863b491eadecac930ad1bf93170be15e
Maic Miller <maicmiller42@gmail.com>
2021-08-25 21:02:42 -0300

    :heavy_check_mark: test(isChrome):add test incorrectPassword

--- disneyPlus/src/test/java/com/disneyPlusTests/LoginDisneyPlusTests.java
+++ disneyPlus/src/test/java/com/disneyPlusTests/LoginDisneyPlusTests.java
@@ -22,4 +22,22 @@ public class LoginDisneyPlusTests {
         $("#password").setValue("johnwick@pencil");
         $("button[name=dssLoginSubmit]").click();
     }
+
+    @Test
+    public void incorrectPassword(){
+
+        isChrome();
+        open("https://www.disneyplus.com/pt-br");
+        $(".nav-initial .wrapper a").shouldHave(text("Entrar")).click();
+
+        $("#email").setValue("johnwick@gmail.com");
+        $("button[data-gv2elementkey]").click();
+
+        $("#password").setValue("johnwick@pencil");
+        $("button[name=dssLoginSubmit]").click();
+
+        $("div[data-testid=text-input-error]").shouldHave(text("Senha incorreta. Digite novamente. Se o " +
+                     "problema continuar, selecione \"Esqueceu a senha?\" e faça a redefinição (código de erro 14)."));
+
+    }
 }
