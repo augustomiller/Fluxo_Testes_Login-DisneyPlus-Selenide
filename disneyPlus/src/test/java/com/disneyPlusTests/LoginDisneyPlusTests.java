85a963d12a6694f6e90b15e8903f610cfe702284
Maic Miller <maicmiller42@gmail.com>
2021-08-25 23:43:32 -0300

    :rotating_light: tests(DDT): Add Data Provider and regression test
    Definindo a Massa de teste que vamos ter como provider. Mais teste de Regressão.

--- disneyPlus/src/test/java/com/disneyPlusTests/LoginDisneyPlusTests.java
+++ disneyPlus/src/test/java/com/disneyPlusTests/LoginDisneyPlusTests.java
@@ -1,13 +1,23 @@
 package com.disneyPlusTests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.isChrome;
 
 public class LoginDisneyPlusTests {
+
+    @DataProvider(name = "data-provider")
+    public Object[][] loginProvider() {
+        return new Object[][]{
+                {"johnwick@gmail.com", "johnwick@pencil", "Senha incorreta. Digite novamente. Se o problema continuar, selecione \"Esqueceu a senha?\" e faça a redefinição (código de erro 14)."},
+                {"", "johnwick@pencil", "Houve um problema na criação da conta. Volte a digitar seu e-mail e senha e tente novamente. Se o problema continuar, entre em contato com o Suporte do Disney+ (código de erro 6)."},
+                {"johnwick@gmail.com", "", "Houve um problema na criação da conta. Volte a digitar seu e-mail e senha e tente novamente. Se o problema continuar, entre em contato com o Suporte do Disney+ (código de erro 6)."},
+                {"notfound@gmail.com", "johnwick@pencil", "Assinar"}
+        };
+    }
 
     @Test
     public void shouldLoggerUser() {
@@ -16,77 +26,35 @@ public class LoginDisneyPlusTests {
         open("https://www.disneyplus.com/pt-br");
         $(".nav-initial .wrapper a").shouldHave(text("Entrar")).click();
 
-        $("#email").setValue("johnwick@gmail.com");
+        $("#email").setValue("maic.global@gmail.com");
         $("button[data-gv2elementkey]").click();
 
-        $("#password").setValue("johnwick@pencil");
-        $("button[name=dssLoginSubmit]").click();
-    }
-
-    @Test
-    public void incorrectPassword() {
-
-        isChrome();
-        open("https://www.disneyplus.com/pt-br");
-        $(".nav-initial .wrapper a").shouldHave(text("Entrar")).click();
-
-        $("#email").setValue("johnwick@gmail.com");
-        $("button[data-gv2elementkey]").click();
-
-        $("#password").setValue("johnwick@pencil");
+        $("#password").setValue("D4r882wk");
         $("button[name=dssLoginSubmit]").click();
 
-        $("div[data-testid=text-input-error]").shouldHave(text("Senha incorreta. Digite novamente. Se o " +
-                "problema continuar, selecione \"Esqueceu a senha?\" e faça a redefinição (código de erro 14)."));
-
+        $("button[kind=primary]").shouldHave(text("REATIVAR ASSINATURA"));
     }
 
-    @Test
-    public void userNotFound() {
+
+    @Test(dataProvider = "data-provider")
+    public void shouldSeeLoginAlerts(String email, String pass, String expectAlert) {
 
         isChrome();
+
+        executeJavaScript("localStorage.clear();");
+
         open("https://www.disneyplus.com/pt-br");
+
+
         $(".nav-initial .wrapper a").shouldHave(text("Entrar")).click();
 
-        $("#email").setValue("notfound@gmail.com");
+        $("#email").setValue(email);
         $("button[data-gv2elementkey]").click();
 
-        $("button[data-testid=modal-secondary-button]").shouldHave(text("Assinar"));
-
-    }
-
-    @Test
-    public void emailRequired() {
-
-        isChrome();
-        open("https://www.disneyplus.com/pt-br");
-        $(".nav-initial .wrapper a").shouldHave(text("Entrar")).click();
-
-        $("#email").setValue("");
-        $("button[data-gv2elementkey]").click();
-
-        $("div[data-testid=text-input-error]").shouldHave(text("Houve um problema na criação da conta. " +
-                " Volte a digitar seu e-mail e senha e tente novamente. Se o problema continuar, entre em contato " +
-                " com o Suporte do Disney+ (código de erro 6)."));
-
-    }
-
-    @Test
-    public void passwordRequired() {
-
-        isChrome();
-        open("https://www.disneyplus.com/pt-br");
-        $(".nav-initial .wrapper a").shouldHave(text("Entrar")).click();
-
-        $("#email").setValue("johnwick@gmail.com");
-        $("button[data-gv2elementkey]").click();
-
-        $("#password").setValue("");
+        $("#password").setValue(pass);
         $("button[name=dssLoginSubmit]").click();
 
-        $("div[data-testid=text-input-error]").shouldHave(text("Houve um problema na criação da conta. " +
-                " Volte a digitar seu e-mail e senha e tente novamente. Se o problema continuar, entre em contato " +
-                " com o Suporte do Disney+ (código de erro 6)."));
+        $("div[data-testid=text-input-error]").shouldHave(text(expectAlert));
 
     }
 }
